f1b27880c14a01fc8189da8e2cc75eab62e8b8cf
Nikolay Kolpicov <nikolay200021@gmail.com>
2021-03-08 21:56:06 +0300

    v4.0.0 pre-alpha (trial of WolfMQTT Implementation)

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,16 @@
 const char BUILD[] = __DATE__ " " __TIME__;
 #define FW_NAME         "Lampan-EVT2"
-#define FW_VERSION      "2.0.1"
-
+#define FW_VERSION      "4.0.0 alpha (WolfMQTT)"
+#define ENABLE_MQTT_TLS
 #define TINY_GSM_MODEM_SIM800
 #define _TASK_STATUS_REQUEST
 #include <Arduino.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_NeoMatrix.h>
-//#include <Adafruit_GFX.h>
-//#include <Adafruit_NeoPixel.h>
 #include <TinyGsmClient.h>
-#include <PubSubClient.h>
-#include <ArduinoJson.h>
 #include <TaskScheduler.h>
+#include <wolfMQTT.h>
 #define MATRIX_PIN PA7
 
 
@@ -27,10 +24,33 @@ const char BUILD[] = __DATE__ " " __TIME__;
 #define SerialMon Serial
 #define SerialAT Serial1
 
+#define DEFAULT_MQTT_HOST       "iot.eclipse.org" /* broker.hivemq.com */
+#define DEFAULT_CMD_TIMEOUT_MS  30000
+#define DEFAULT_CON_TIMEOUT_MS  5000
+#define DEFAULT_MQTT_QOS        MQTT_QOS_0
+#define DEFAULT_KEEP_ALIVE_SEC  60
+#define DEFAULT_CLIENT_ID       "WolfMQTTClient"
+#define WOLFMQTT_TOPIC_NAME     "wolfMQTT/example/"
+#define DEFAULT_TOPIC_NAME      WOLFMQTT_TOPIC_NAME"testTopic"
+#define MAX_BUFFER_SIZE         1024
+#define TEST_MESSAGE            "test"
+#define TEST_TOPIC_COUNT        2
+
+#ifdef ENABLE_MQTT_TLS
+static WOLFSSL_METHOD* mMethod = 0;
+static WOLFSSL_CTX* mCtx       = 0;
+static WOLFSSL* mSsl           = 0;
+static const char* mTlsFile    = NULL;
+#endif
+static word16 mPort            = 0;
+static const char* mHost       = "iot.eclipse.org";
+static int mStopRead    = 0;
 //Classes definition
 TinyGsm modem(SerialAT);
-TinyGsmClient client(modem);
-PubSubClient mqtt(client);
+TinyGsmClient modemclient(modem);
+MqttClient mqttclient;
+MqttNet net;
+
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(16, 16, MATRIX_PIN,
                             NEO_MATRIX_BOTTOM     + NEO_MATRIX_LEFT +
                             NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
@@ -100,21 +120,173 @@ void SignalTest();
 Task tNotification(TASK_IMMEDIATE, TASK_FOREVER, &NotiCallback, &ts,false, &NotiOnEnable, &NotiOnDisable);
 Task tRSSI(ServTime*TASK_SECOND, TASK_FOREVER, &PublishRSSI, &ts);
 Task tSignalTest(TASK_IMMEDIATE, TASK_FOREVER, &SignalTest, &ts);
+static int mqttclient_tls_verify_cb(int preverify, WOLFSSL_X509_STORE_CTX* store)
+{
+  char buffer[WOLFSSL_MAX_ERROR_SZ];
+
+  printf("MQTT TLS Verify Callback: PreVerify %d, Error %d (%s)\n", preverify,
+         store->error, wolfSSL_ERR_error_string(store->error, buffer));
+  printf("  Subject's domain name is %s\n", store->domain);
+
+  if (store->error != 0) {
+    /* Allowing to continue */
+    /* Should check certificate and return 0 if not okay */
+    printf("  Allowing cert anyways");
+  }
+
+  return 1;
+}
+
+static int mqttclient_tls_cb(MqttClient* cli)
+{
+  int rc = WOLFSSL_FAILURE;
+  
+  cli->tls.ctx = wolfSSL_CTX_new(wolfTLSv1_2_client_method());
+  if (cli->tls.ctx) {
+    wolfSSL_CTX_set_verify(cli->tls.ctx, SSL_VERIFY_PEER, mqttclient_tls_verify_cb);
+
+    /* default to success */
+    rc = WOLFSSL_SUCCESS;
+
+    if (mTlsFile) {
+      /* Load CA certificate file */
+      rc = wolfSSL_CTX_load_verify_locations(cli->tls.ctx, mTlsFile, 0);
+    }
+  }
+  else {
+#if 0
+    /* Load CA using buffer */
+    rc = wolfSSL_CTX_load_verify_buffer(cli->tls.ctx, caCertBuf,
+                                        caCertSize, WOLFSSL_FILETYPE_PEM);
+#endif
+    rc = WOLFSSL_SUCCESS;
+  }
+
+  printf("MQTT TLS Setup (%d)\n", rc);
+
+  return rc;
+}
+static int SimConnect(void *context, const char* host, word16 port, int timeout_ms) 
+{
+  int ret = 0;
+
+  ret = modemclient.connect(host, port);
+
+  return ret;
+}
+
+static int SimRead(void *context, byte* buf, int buf_len, int timeout_ms) 
+{
+  int recvd = 0;
+  /* While data and buffer available */
+  while (modemclient.available() > 0 && recvd < buf_len) {
+    buf[recvd] = modemclient.read();
+    recvd++;
+  }
+
+  return recvd;
+}
+
+static int SimWrite(void *context, const byte* buf, int buf_len, int timeout_ms) 
+{
+  int sent = 0;
+
+  sent = modemclient.write(buf, buf_len);
+
+  return sent;
+}
+
+static int SimDisconnect(void *context) 
+{
+  modemclient.stop();
+
+  return 0;
+}
+#define MAX_PACKET_ID   ((1 << 16) - 1)
+static int mPacketIdLast;
+static word16 mqttclient_get_packetid(void)
+{
+  mPacketIdLast = (mPacketIdLast >= MAX_PACKET_ID) ?
+                  1 : mPacketIdLast + 1;
+  return (word16)mPacketIdLast;
+}
+
+#define PRINT_BUFFER_SIZE    80
+static int mqttclient_message_cb(MqttClient *client, MqttMessage *msg,
+                                 byte msg_new, byte msg_done)
+{
+  byte buf[PRINT_BUFFER_SIZE + 1];
+  word32 len;
+
+  (void)client; /* Supress un-used argument */
+
+  if (msg_new) {
+    /* Determine min size to dump */
+    len = msg->topic_name_len;
+    if (len > PRINT_BUFFER_SIZE) {
+      len = PRINT_BUFFER_SIZE;
+    }
+    XMEMCPY(buf, msg->topic_name, len);
+    buf[len] = '\0'; /* Make sure its null terminated */
+
+    /* Print incoming message */
+    Serial.print("MQTT Message: Topic ");
+    Serial.println((char*)buf);
+    Serial.print("Qos ");
+    Serial.println(msg->qos);
+    Serial.print("Len ");
+    Serial.println(msg->total_len);
+  }
+
+  /* Print message payload */
+  len = msg->buffer_len;
+  if (len > PRINT_BUFFER_SIZE) {
+    len = PRINT_BUFFER_SIZE;
+  }
+  XMEMCPY(buf, msg->buffer, len);
+  buf[len] = '\0'; /* Make sure its null terminated */
+  Serial.print("Payload: ");
+  Serial.println((char*)buf);
+
+  if (msg_done) {
+    Serial.println("MQTT Message: Done");
+  }
+
+  /* Return negative to terminate publish processing */
+  return MQTT_CODE_SUCCESS;
+}
+
 bool mqttConnect()
 {
-  while (!mqtt.connected())
-  {
-    SerialMon.print(F("MQTT?"));
-    if (mqtt.connect(DeviceID, mqtt_user, mqtt_pass))
-    {
-      Serial.println(F(" OK"));
-      mqtt.publish(topicRegister, DeviceID,16);
-      int csq = modem.getSignalQuality();
-      int RSSI = -113 +(csq*2);
-      char serrssi[4];
-      itoa(RSSI, serrssi, 10);
-      mqtt.publish(topicService, serrssi);
-      mqtt.subscribe(topicStatus,1);
+    MqttConnect connect;
+    memset(&connect, 0, sizeof(MqttConnect));
+    connect.keep_alive_sec = keep_alive_sec;
+    connect.clean_session = clean_session;
+    connect.client_id = DeviceID;
+    connect.username = mqtt_user;
+    connect.password = mqtt_pass;
+    MqttClient_Connect(&mqttclient, &connect);
+      MqttSubscribe subscribe;
+      MqttTopic topics[1], *topic;
+      MqttPublish publish;
+
+      memset(&publish, 0, sizeof(MqttPublish));
+      publish.retain = 0;
+      publish.qos = MQTT_QOS_0;
+      publish.duplicate = 0;
+      publish.topic_name = topicRegister;
+      publish.packet_id = mqttclient_get_packetid();
+      publish.buffer = (byte*)DeviceID;
+      publish.total_len = (word16)strlen(DeviceID);
+      MqttClient_Publish(&mqttclient, &publish);
+
+      topics[0].topic_filter = topicStatus;
+      topics[0].qos = MQTT_QOS_1;
+      memset(&subscribe, 0, sizeof(MqttSubscribe));
+      subscribe.packet_id = mqttclient_get_packetid();
+      subscribe.topic_count = sizeof(topics)/sizeof(MqttTopic);
+      subscribe.topics = topics;
+      rc = MqttClient_Subscribe(&mqttclient, &subscribe);
       if (!IsSetupComplete) //Add Boot is Complete Animation
       {
         IsSetupComplete = true;
@@ -205,7 +377,6 @@ void setup()
   String modemInfo = modem.getModemInfo();
   SerialMon.print(F("Modem Info: "));
   SerialMon.println(modemInfo);
-
   DeviceImei = modem.getIMEI();
   DeviceImei.toCharArray(DeviceID, 16);
   SerialMon.print("Lamp ID number: ");
@@ -268,19 +439,36 @@ void setup()
     matrix.show();
 
   }
+  MqttQoS qos = DEFAULT_MQTT_QOS;
+  byte clean_session = 1;
+  word16 keep_alive_sec = 60;
+  int enable_lwt = 0;
+  byte tx_buf[MAX_BUFFER_SIZE];
+  byte rx_buf[MAX_BUFFER_SIZE];
+  MqttMsgCb msg_cb;
+  net.connect = SimConnect;
+  net.read = SimRead;
+  net.write = SimWrite;
+  net.disconnect = SimDisconnect;
+  net.context = &modemclient;
+
+  MqttClient_Init(&mqttclient, &net, mqttclient_message_cb, tx_buf, MAX_BUFFER_SIZE,
+                       rx_buf, MAX_BUFFER_SIZE, DEFAULT_CMD_TIMEOUT_MS);
+
+  int rc = MqttClient_NetConnect(&mqttclient, mHost, mPort,
+                             DEFAULT_CON_TIMEOUT_MS, true,
+                             mqttclient_tls_cb);
   ts.addTask(tNotification);
   ts.addTask(tRSSI);
   
   tRSSI.enable();
   // MQTT Broker setup
   SerialMon.println(F("SETUP"));
-  mqtt.setServer(broker, 1883);
-  mqtt.setCallback(mqttRX);
   matrix.fillRect(0,0,14,16,PBColour);
   matrix.show();
-  mqttConnect();
   //ServTimer = millis();
 }
+
 
 void loop()
 {
