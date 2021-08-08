e3a760198c3dbbb8a556b118627adbfb4372a845
Nikolay Kolpicov <nikolay200021@gmail.com>
2021-08-08 04:31:25 +0300

    Revert "v3.0.0 alpha 2"
    This reverts commit 07cadbb01a7b046e3b8582d65339cf77c6e032bc.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 const char BUILD[] = __DATE__ " " __TIME__;
 #define FW_NAME         "Lampan DVT4"
-#define FW_VERSION      "v2.2.1 Master"
+#define FW_VERSION      "v2.2.2 Master (Chase-added)"
 #define TINY_GSM_MODEM_SIM800
 #define ARDUINOJSON_USE_LONG_LONG 1
 
@@ -30,9 +30,9 @@ Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(16, 16, MATRIX_PIN,
 Scheduler ts;
 
 //APN Credentials
-const char* apn = "mts";
-const char* gprsUser = "mts";
-const char* gprsPass = "mts";
+const char* apn = "internet.beeline.ru";
+const char* gprsUser = "beeline";
+const char* gprsPass = "beeline";
 // MQTT details
 const char* broker = "mqtt.iotcreative.ru";
 const char* mqtt_user = "dave";
@@ -43,7 +43,8 @@ char topicCmd[32]= "$device/";
 char topicEvent[45]= "$device/";
 int LDC = 0;
 //Pulse anim
-const byte PulseDelay = 40;
+const byte PulseFD = 10; //Frequency Delay 
+const byte AnimFreq = 2;
 //Mixed anim
 bool ColCh = true;
 uint32_t MixedColour = matrix.Color(255, 165, 0); //Mixed Notification Colour
@@ -51,8 +52,10 @@ uint32_t MixedColour = matrix.Color(255, 165, 0); //Mixed Notification Colour
 const uint32_t PBColour = matrix.Color(255,255,255); //Progress Bar Colour
 const byte MainBrightness = 40;
 byte NotiBrightness = 100;
+//Chase anim
+const uint8_t ChaseFD = 70;
 //Wave anim
-float AnimDelay = 2000;
+float AnimDelay = 300;
 const byte MinBrightness = 20;
 uint32_t NSColour = matrix.Color(255, 255, 255); //Notification Strip Colour
 uint32_t NSGColour = matrix.Color(180, 255, 180); //Notification Gradient Colour 
@@ -66,6 +69,7 @@ bool NotiOn = false;
 bool IsSetupComplete = false;
 char DeviceID[16];
 uint32_t StateFreq = 1; //Minutes
+bool AutoReset = true;
 //Error variables
 byte NetRT = 0;
 byte GprsRT = 0;
@@ -86,12 +90,18 @@ void FadeOut(uint32_t fadecolour, int brightness);
 bool NotiOnEnable(); 
 void NotiCallback();
 void NotiOnDisable();
-//Animation: 0 - pulse; 1 - mixed
-byte pattern = 0;
+//Current mode: 0 - pulse; 1 - mixed; 2 - test; 3 - off;
+//Default Animation: 0 - pulse; 1 - wave; 2 - chase (EXPERIMENTAL - OVERHEATING)
+byte CurrentMode = 0;
+byte DefaultAnimation = 0;
 void Light(); 
-void wave();
+void wave(uint32_t Colour);
 void pulse(uint32_t Colour);
-void mixed();
+void mixedPulse();
+void mixedWave();
+void chase(uint32_t Colour1, uint32_t Colour2);
+void chaseOff(uint32_t Colour1, uint32_t Colour2);
+void chaseOn(uint32_t Colour1, uint32_t Colour2);
 //Service functions
 void ProgressBar (int pb);
 void error(int errcode);
@@ -105,7 +115,7 @@ void button();
 //Task 1 - Notification
 //Task 2 - Publish State
 //Task 3 - Signal Test
-Task tNotification(TASK_IMMEDIATE, TASK_FOREVER, &NotiCallback, &ts,false, &NotiOnEnable, &NotiOnDisable);
+Task tNotification(AnimFreq*TASK_SECOND, TASK_FOREVER, &NotiCallback, &ts,false, &NotiOnEnable, &NotiOnDisable);
 Task tState(StateFreq*TASK_MINUTE, TASK_FOREVER, &PublishState, &ts);
 Task tSignalTest(TASK_IMMEDIATE, TASK_FOREVER, &SignalTest, &ts, false, &SignalTestEn, &SignalTestDis);
 
@@ -212,6 +222,8 @@ void setup()
   {
     error(5);
   }
+  int RSSI = modem.getSignalQuality();
+  SerialMon.println(RSSI);
   SerialMon.print("NET? ");
   if (!modem.waitForNetwork())
   {
@@ -267,6 +279,15 @@ void loop()
 bool NotiOnEnable()
 {
     //Fadein(NotiColour, NotiBrightness);
+    if(DefaultAnimation == 2 && CurrentMode == 0)
+    {
+       chaseOn(NotiColour, matrix.Color(255,255,255));
+    }
+    else if(DefaultAnimation == 2 && CurrentMode == 1)
+    {
+       chaseOn(NotiColour, MixedColour);
+    }
+    NotiOn = false;
     NotiOn = true;
     SerialMon.println(F("Notification enabled"));
     return true;
@@ -274,16 +295,39 @@ bool NotiOnEnable()
 void NotiOnDisable()
 {
     //FadeOut(NotiColour, NotiBrightness);
+    if(DefaultAnimation == 2 && CurrentMode == 0)
+    {
+       chaseOff(NotiColour, matrix.Color(255,255,255));
+    }
+    else if(DefaultAnimation == 2 && CurrentMode == 1)
+    {
+       chaseOff(NotiColour, MixedColour);
+    }
     NotiOn = false;
 }
 void NotiCallback()
 {
-      switch(pattern)
+  
+      switch(CurrentMode)
       {
-          case 0: pulse(NotiColour); break;
-          case 1: mixed(); break;
+          case 0: 
+            switch(DefaultAnimation)
+            {
+              case 0: pulse(NotiColour); break;
+              case 1: wave(NotiColour); break;
+              case 2: chase(NotiColour, matrix.Color(255,255,255)); break;
+            }
+          break;
+          case 1: 
+            switch(DefaultAnimation)
+            {
+              case 0: mixedPulse(); break;
+              case 1: mixedWave(); break;
+              case 2: chase(NotiColour, MixedColour); break;
+            } break;
       }
 }
+
 //Signal Test Task
 void SignalTest ()
 {
@@ -310,7 +354,7 @@ void SignalTest ()
     }
     else if (RSSI >=  -70)
     {
-        matrix.fillScreen(matrix.Color(0,0,255));
+        matrix.fillScreen(matrix.Color(0,255,0));
         matrix.show();    
         }
 }
@@ -350,7 +394,7 @@ void PublishState ()
       StaticJsonDocument<256> state;
       if(NotiOn)
       {
-          switch(pattern)
+          switch(CurrentMode)
           {
             case 0: state["mode"] = "pulse"; break;
             case 1: state["mode"] = "mixed";  state["color2"] = CurrentMixedColour; break;
@@ -390,52 +434,52 @@ void JSONApply(byte *payload, unsigned int len)
     {
       TestMode = false;
       tSignalTest.disable();
-
+      
           //Colour change
           String CC = config["color"];
-          CC.toCharArray(CurrentColour,13);
+          char NewColour1[13];
+          CC.toCharArray(NewColour1,13);
           uint64_t colour = config["color"];
           int r = colour / 1000000000;
           int g = (colour % 1000000000)/1000000;
           int b = (colour % 1000000)/1000;
           int a = colour % 1000;
-          SerialMon.print(F("Got colour (RGBA): "));
-          SerialMon.print(r);
-          SerialMon.print(F(", "));
-          SerialMon.print(g);
-          SerialMon.print(F(", "));
-          SerialMon.print(b);
-          SerialMon.print(F(", "));
-          SerialMon.println(a);
-          NotiColour = matrix.Color(r,g,b);
-          NotiBrightness = a;
+          if (NewColour1 != CurrentColour)
+          {
+              tNotification.disable();
+              NotiColour = matrix.Color(r,g,b);
+              CC.toCharArray(CurrentColour,13);
+              NotiBrightness = a;
+              if(config["mode"] == "pulse")
+              {
+                  CurrentMode = 0;
+                  tNotification.enable();
+              }
+          }
           if (config["mode"] == "mixed")
           {
             SerialMon.println(F("Mixed mode"));
             String CC2 = config["color2"];
-            CC2.toCharArray(CurrentMixedColour,13);
+            char NewColour2[13];
+            CC2.toCharArray(NewColour2,13);
             uint64_t colour2 = config["color2"];
             int r2 = colour2 / 1000000000;
             int g2 = (colour2 % 1000000000)/1000000;
             int b2 = (colour2 % 1000000)/1000;
-            int a2 = colour2 % 1000;
-            SerialMon.print(F("Got colour 2 (RGBA): "));
-            SerialMon.print(r2);
-            SerialMon.print(F(", "));
-            SerialMon.print(g2);
-            SerialMon.print(F(", "));
-            SerialMon.print(b2);
-            SerialMon.print(F(", "));
-            SerialMon.println(a2);
-            MixedColour = matrix.Color(r2,g2,b2);
-            pattern = 1;
+            if (NewColour2 != CurrentMixedColour)
+            {
+              tNotification.disable();
+              CC2.toCharArray(CurrentMixedColour,13);
+              MixedColour = matrix.Color(r2,g2,b2);
+              tNotification.enable();
+            }
+            if(CurrentMode != 1)
+            {
+                CurrentMode = 1;
+                tNotification.enable();
+            }
+            
           }
-          else if (config["mode"] == "pulse")
-          {
-              SerialMon.println(F("Pulse mode"));
-              pattern = 0;
-          }
-          tNotification.enable();
           //Colour change end
     }
     else if (config["mode"] == "test")
@@ -444,6 +488,41 @@ void JSONApply(byte *payload, unsigned int len)
       TestMode = true;
       tNotification.disable();
       tSignalTest.enable();
+    }
+     else if(config["mode"]=="write-defaults")
+    {
+        SerialMon.println(F("Write-defaults mode"));
+        if(config["process"]=="Notification")
+        { 
+            if(config["-a"]=="pulse")
+            {
+                DefaultAnimation = 0;
+            }
+            else if(config["-a"]=="wave")
+            {
+                DefaultAnimation = 1;
+            }
+            else if(config["-a"]=="chase")
+            {
+                DefaultAnimation = 2;
+            }
+        }
+        if(config["process"]=="State")
+        {
+            StateFreq = config["-t"];
+        }
+        if(config["process"]=="System")
+        {
+            if(config["AutoReset"]=="true")
+            {
+                AutoReset = true;
+            }
+            else if(config["AutoReset"]=="false")
+            { 
+                AutoReset = false;
+            }
+        }
+        
     }
     if(config["mode"]=="off")
     {
@@ -458,10 +537,10 @@ void error(int errcode)
 {
     Serial.println("Error " + errcode);
     delay(1000);
-    while(1)
+    int i = 0;
+    while(i < 5)
     {
       matrix.fillScreen(matrix.Color(255,0,0));
-      
       matrix.show();
       delay(2000);
       switch (errcode)
@@ -511,7 +590,12 @@ void error(int errcode)
           delay(2000);
           break;
       }
+      if (AutoReset)
+      {
+        i++;
+      }
     }
+    NVIC_SystemReset();
 }
 void ProgressBar (int pb)
 {
@@ -527,19 +611,21 @@ void Fadein(uint32_t fadecolor, int brightness)
   matrix.show();
   for (byte j = 0; j < brightness; j++)
   {
-    matrix.setBrightness(j);
-    delay(1);
     matrix.fillScreen(fadecolor);
+    matrix.setBrightness(j);
     matrix.show();
+    delay(PulseFD);
   }
 }
 void FadeOut(uint32_t fadecolour, int brightness)
 {
   //matrix.setBrightness(brightness);
-  for (byte k = brightness; k > 0; k-=2)
+  for (byte k = brightness; k > 0; k--)
   {
+    matrix.fillScreen(fadecolour);
     matrix.setBrightness(k);
-    delay(1);
+    matrix.show();
+    delay(PulseFD);
   }
   matrix.fillScreen(matrix.Color(0, 0, 0));
   matrix.show();
@@ -559,36 +645,16 @@ void Light()
 }
 void pulse(uint32_t Colour)
 {
-  for (byte i = 0; i <= NotiBrightness; i++)
-  {
-    matrix.fillScreen(Colour);
-    if (i < (NotiBrightness/2))
-    {
-      matrix.setBrightness(i);
-      matrix.show();
-      tNotification.delay(PulseDelay);
-    }
-    else if (i == (NotiBrightness/2))
-    {
-      matrix.setBrightness(i);
-      matrix.show();
-      tNotification.delay(AnimDelay);
-    }
-    else
-    {
-      matrix.setBrightness(NotiBrightness - i);
-      matrix.show();
-      tNotification.delay(PulseDelay);
-    }
-  }
-  tNotification.delay(AnimDelay);
+    Fadein(Colour, NotiBrightness);
+    delay(AnimDelay);
+    FadeOut(Colour, NotiBrightness);
 }
-void wave()
+void wave(uint32_t Colour)
 {
-  float brc = (NotiBrightness - MinBrightness) / 8;
+  float brc = NotiBrightness / 8;
   for (byte i = 0; i <= 17; i++)
   {
-    matrix.fillScreen(NotiColour);
+    matrix.fillScreen(Colour);
     matrix.drawLine(i, 0, i, 16, NSColour);
     matrix.drawLine(i + 1, 0, i + 1, 16, NSGColour);
     matrix.drawLine(i - 1, 0, i - 1, 16, NSGColour);
@@ -596,18 +662,21 @@ void wave()
     if (i <= 8)
     {
       //delay(delays-(i*delayk));
-      matrix.setBrightness(MinBrightness+ (i * brc));
-      delay(PulseDelay);
+      matrix.setBrightness(i+(i * brc));
+      delay(40);
     }
     else
     {
       // delay(delays-(8*delayk)+(i*delayk));
-      matrix.setBrightness(NotiBrightness - (i - 8)*brc);
-      delay(PulseDelay);
+      
+      matrix.setBrightness(NotiBrightness - ((i - 8)*brc));
+      delay(40);
     }
   }
+  matrix.setBrightness(0);
+  matrix.show();
 }
-void mixed()
+void mixedPulse()
 {
   switch (ColCh)
   {
@@ -616,4 +685,143 @@ void mixed()
   }
   ColCh = !ColCh;
 }
+void mixedWave()
+{
+  switch (ColCh)
+  {
+     case true: wave(NotiColour); break;
+     case false: wave(MixedColour); break;
+  }
+  ColCh = !ColCh;
+}
+void chase(uint32_t Colour1, uint32_t Colour2)
+{
+      matrix.setBrightness(NotiBrightness);
+      
+      for(int i = 0; i < 16; i++)
+      {
+      matrix.fillScreen(Colour2);
 
+      matrix.drawLine(i-24,0,i-16,16,Colour1);
+      matrix.drawLine(i-23,0,i-15,15,Colour1);
+      matrix.drawLine(i-22,0,i-14,15,Colour1);
+      matrix.drawLine(i-21,0,i-13,15,Colour1);
+
+      matrix.drawLine(i-16 ,0,i-8,15,Colour1);
+      matrix.drawLine(i-15,0,i-7,15,Colour1);
+      matrix.drawLine(i-14,0,i-6,15,Colour1);
+      matrix.drawLine(i-13,0,i-5,15,Colour1);
+
+      matrix.drawLine(i-8,0,i,15,Colour1);
+      matrix.drawLine(i-7,0,i+1,15,Colour1);
+      matrix.drawLine(i-6,0,i+2,15,Colour1);
+      matrix.drawLine(i-5,0,i+3,15,Colour1);
+
+
+      matrix.drawLine(i ,0,i+8,15,Colour1);
+      matrix.drawLine(i+ 1,0,i+9,15,Colour1);
+      matrix.drawLine(i+ 2,0,i+10,15,Colour1);
+      matrix.drawLine(i + 3,0,i+11,15,Colour1);
+
+ 
+
+      matrix.drawLine(i+8,0,i+16,15,Colour1);
+      matrix.drawLine(i+9,0,i+17,15,Colour1);
+      matrix.drawLine(i+10,0,i+18,15,Colour1);
+      matrix.drawLine(i+11,0,i+19,15,Colour1);
+
+
+      matrix.show();
+      delay(ChaseFD);
+      }
+      SerialMon.println("Chase!");
+}
+void chaseOff(uint32_t Colour1, uint32_t Colour2)
+{
+      int CurrentBr = NotiBrightness;
+      matrix.setBrightness(NotiBrightness);
+      int brc = NotiBrightness / 16;
+      for(int i = 0; i < 16; i++)
+      {
+      matrix.fillScreen(Colour2);
+
+      matrix.drawLine(i-24,0,i-16,16,Colour1);
+      matrix.drawLine(i-23,0,i-15,15,Colour1);
+      matrix.drawLine(i-22,0,i-14,15,Colour1);
+      matrix.drawLine(i-21,0,i-13,15,Colour1);
+
+      matrix.drawLine(i-16 ,0,i-8,15,Colour1);
+      matrix.drawLine(i-15,0,i-7,15,Colour1);
+      matrix.drawLine(i-14,0,i-6,15,Colour1);
+      matrix.drawLine(i-13,0,i-5,15,Colour1);
+
+      matrix.drawLine(i-8,0,i,15,Colour1);
+      matrix.drawLine(i-7,0,i+1,15,Colour1);
+      matrix.drawLine(i-6,0,i+2,15,Colour1);
+      matrix.drawLine(i-5,0,i+3,15,Colour1);
+
+
+      matrix.drawLine(i ,0,i+8,15,Colour1);
+      matrix.drawLine(i+ 1,0,i+9,15,Colour1);
+      matrix.drawLine(i+ 2,0,i+10,15,Colour1);
+      matrix.drawLine(i + 3,0,i+11,15,Colour1);
+
+ 
+
+      matrix.drawLine(i+8,0,i+16,15,Colour1);
+      matrix.drawLine(i+9,0,i+17,15,Colour1);
+      matrix.drawLine(i+10,0,i+18,15,Colour1);
+      matrix.drawLine(i+11,0,i+19,15,Colour1);
+      matrix.setBrightness(CurrentBr);
+      matrix.show();
+      delay(ChaseFD);
+      CurrentBr=CurrentBr-brc;
+      }
+      matrix.setBrightness(0);
+      matrix.show();
+      SerialMon.println("Chase! off");
+}
+void chaseOn(uint32_t Colour1, uint32_t Colour2)
+{
+      matrix.setBrightness(0);
+      matrix.show();
+      int brc = NotiBrightness / 16;
+      int CurrentBr = 0;
+      SerialMon.println("Chase! on");
+      for(int i = 0; i < 16; i++)
+      {
+      matrix.fillScreen(Colour2);
+
+      matrix.drawLine(i-24,0,i-16,16,Colour1);
+      matrix.drawLine(i-23,0,i-15,15,Colour1);
+      matrix.drawLine(i-22,0,i-14,15,Colour1);
+      matrix.drawLine(i-21,0,i-13,15,Colour1);
+
+      matrix.drawLine(i-16 ,0,i-8,15,Colour1);
+      matrix.drawLine(i-15,0,i-7,15,Colour1);
+      matrix.drawLine(i-14,0,i-6,15,Colour1);
+      matrix.drawLine(i-13,0,i-5,15,Colour1);
+
+      matrix.drawLine(i-8,0,i,15,Colour1);
+      matrix.drawLine(i-7,0,i+1,15,Colour1);
+      matrix.drawLine(i-6,0,i+2,15,Colour1);
+      matrix.drawLine(i-5,0,i+3,15,Colour1);
+
+
+      matrix.drawLine(i ,0,i+8,15,Colour1);
+      matrix.drawLine(i+ 1,0,i+9,15,Colour1);
+      matrix.drawLine(i+ 2,0,i+10,15,Colour1);
+      matrix.drawLine(i + 3,0,i+11,15,Colour1);
+
+ 
+
+      matrix.drawLine(i+8,0,i+16,15,Colour1);
+      matrix.drawLine(i+9,0,i+17,15,Colour1);
+      matrix.drawLine(i+10,0,i+18,15,Colour1);
+      matrix.drawLine(i+11,0,i+19,15,Colour1);
+      matrix.setBrightness(CurrentBr);
+      matrix.show();
+      delay(ChaseFD);
+      CurrentBr=CurrentBr+brc;
+      }
+}
